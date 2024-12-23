200173d1218999d9fa1c214708ec97732f00f952
arora <arora.geeks@gmail.com>
2024-12-23 21:26:21 +0530

    embedding support added

--- chatgpt_client.py
+++ chatgpt_client.py
@@ -1,28 +1,80 @@
-import openai;
+import openai
+import httpcore
 import time
+import httpx
 
-openai.api_key = "sk-proj-xx";
+openai.api_key = "sk-proj-xx"
+# openai.timeout = 60
+retries = 1
 
-def generate_chatgpt_response(prompt):
+def generate_chatgpt_chat_response(prompt):
+    for i in range(retries):
+        try:
+            if __name__ == "chatgpt_client":
+                response = openai.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=prompt,
+                    max_tokens=10,
+                    temperature=0.7,
+                    n=1,
+                    stop=["\n"]
+                )
+                print(response)
+                return response
+            print("empty")
+            return "empty"
+        except httpcore.ConnectTimeout:
+            print(f"httpcore Timeout, retry attempt: {i}")
+            time.sleep(5)
+        except httpx.ConnectTimeout:
+            print(f"httpx Timeout, retry attempt: {i}")
+            time.sleep(5)
+        except openai.error.OpenAIError as e:
+            print(f"OpenAIError catch: {e}")
+        except openai.BadRequestError as e:
+            print(f"bad request catch: Error occured {e}")
+        except Exception as e:
+            print(f"last catch: {e}")
+
+def generate_chatgpt_response(promptt):
     # Every Python module (file) has a special built-in variable called __name__.
     # When a file is run directly, the value of __name__ is set to "__main__".
     # When a file is imported as a module, the value of __name__ is set to the module's name.
     print(__name__)  # //chatgpt_client
-    try:
-        if __name__ == "__main__":
-            response = openai.chat.completions.create(
-                model="gpt-3.5-turbo",
-                messages=prompt,
-                max_tokens=10,
-                temperature=0.7,
-                n=1,
-                stop=["\n"]
-            )
-            print(response)
-            return response
-        print("empty")
-        return "empty"
-    except openai.BadRequestError:
-        print("Error occured")
-    except Exception as e:
-        print(f"{e}")
+    for i in range(retries):
+        try:
+            if __name__ == "chatgpt_client":
+                response = openai.completions.create(
+                    model="gpt-3.5-turbo-instruct",
+                    prompt=promptt,
+                    max_tokens=10,
+                    temperature=0.7,
+                    n=1,
+                    stop=["\n"]
+                )
+                print(response)
+                return response
+            print("empty")
+            return "empty"
+        except httpcore.ConnectTimeout:
+            print(f"httpcore Timeout, retry attempt: {i}")
+            time.sleep(5)
+        except httpx.ConnectTimeout:
+            print(f"httpx Timeout, retry attempt: {i}")
+            time.sleep(5)
+        except openai.error.OpenAIError as e:
+            print(f"OpenAIError catch: {e}")
+        except openai.BadRequestError as e:
+            print(f"bad request catch: Error occured {e}")
+        except Exception as e:
+            print(f"last catch: {e}")
+
+def generate_embed(prompt):
+    response = openai.embeddings.create(
+        model="text-embedding-ada-002",
+        input=prompt
+    )
+    pure_embeds =[]
+    for embedding in response.data:
+        pure_embeds.append(embedding.embedding)
+    return pure_embeds
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from chatgpt_client import generate_chatgpt_response
+from chatgpt_client import generate_embed
+from chatgpt_client import generate_chatgpt_chat_response
 
+selected_option = input("Select one option: 1. completion 2. embedding 3. chat")
 while True:
     user_imput= input("Enter input: ")
     if(user_imput=="q"):
         break
-    generate_chatgpt_response(user_imput)
+    if(selected_option == "1"):
+     generate_chatgpt_response(user_imput)
+    if(selected_option == "2"):
+       generate_embed(user_imput)
+    if(selected_option == "3"):
+       generate_chatgpt_chat_response(user_imput)
