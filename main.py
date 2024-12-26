8e870230c746831418e151ac3f2e113a0cc70ebb
arora <arora.geeks@gmail.com>
2024-12-26 17:08:27 +0530

    load csv using panda

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from chatgpt_client import generate_chatgpt_response
 from chatgpt_client import generate_embed
 from chatgpt_client import generate_chatgpt_chat_response
+import pandas as pd
 
-selected_option = input("Select one option: 1. completion 2. embedding 3. chat")
+selected_option = input("Select one option: 1. completion 2. embedding 3. chat 4. load movie csv data")
 while True:
     user_imput= input("Enter input: ")
     if(user_imput=="q"):
@@ -13,3 +14,11 @@ while True:
        generate_embed(user_imput)
     if(selected_option == "3"):
        generate_chatgpt_chat_response(user_imput)
+    if(selected_option == "4"):
+       input = "movies.csv" if input == "d" else input
+       dataset = pd.read_csv(input, low_memory=False)
+       subset = dataset[['title','overview']]
+       subset.dropna(inplace=True)
+       print(subset)
+    else:
+       print("end")
